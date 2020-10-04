f4f4065ca13824a930191d3c62f710c92e07e081
Dima <woicdv@gmail.com>
2020-10-04 19:50:52 +0400

    Добавлена функция запуска бота через сигнал модели post_save

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-import instapy
+from instapy import InstaPy
+import os
 
 
 class InstaLikeBot(models.Model):
+    is_active = models.BooleanField(verbose_name='Бот активен', default=False)
     name = models.CharField(verbose_name='Имя бота')
     login = models.CharField(verbose_name='логин')
     password = models.CharField(verbose_name='пароль')
@@ -23,4 +25,23 @@ class InstaLikeBot(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def like_bot(login, password, peak_likes_h, peak_likes_daily, like_tags, like_posts, random, rel_enable, rel_max_follow, dont_like):
+    path = os.getcwd() + '/geckodriver'
+    session = InstaPy(username=login, password=password, geckodriver_path=path, headless_browser=True)
+    session.login()
+    session.set_quota_supervisor(enabled=True, peak_likes_hourly=peak_likes_h, peak_likes_daily=peak_likes_daily)
+    session.like_by_tags([like_tags], amount=like_posts, randomize=random)
+    session.set_relationship_bounds(enabled=rel_enable, max_followers=rel_max_follow)
+    session.set_dont_like([dont_like])
+    session.end()
+
+@receiver(post_save, InstaLikeBot)
+def start_bot():
+    bot = InstaLikeBot()
+    if bot.is_active == True:
+        like_bot(bot.login, bot.password, bot.peak_likes_h, bot.peak_likes_d, bot.like_tags, bot.like_posts,
+                 bot.random, bot.relashionships_enable, bot.relashionships_max_follow, bot.dont_like)
+
 
