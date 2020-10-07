0642d7bc501cfb82a890c08494f8f1d5e176e2c7
Dima <woicdv@gmail.com>
2020-10-07 22:05:49 +0400

    добавлены конкурентные потоки при помощи gevent в instalike

--- management/commands/instalike.py
+++ management/commands/instalike.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from instalike.models import InstaLikeBot
 from instapy import InstaPy
 import os
+import gevent
 
 
 def like_bot(login, password, peak_likes_h, peak_likes_daily, like_tags, like_posts, random, rel_enable, rel_max_follow, dont_like):
@@ -16,11 +17,27 @@ def like_bot(login, password, peak_likes_h, peak_likes_daily, like_tags, like_po
     session.end()
 
 
+def bots_i(i):
+    bots = InstaLikeBot.objects.all()
+    if bots[i].is_active:
+        like_bot(bots[i].login, bots[i].password, bots[i].peak_likes_h, bots[i].peak_likes_d, bots[i].like_tags, bots[i].like_posts,
+                 bots[i].random, bots[i].relashionships_enable, bots[i].relashionships_max_follow, bots[i].dont_like)
+        gevent.sleep(0)
+
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         bots = InstaLikeBot.objects.all()
-        for bot in bots:
-            if bot.is_active:
-                like_bot(bot.login, bot.password, bot.peak_likes_h, bot.peak_likes_d, bot.like_tags, bot.like_posts,
-                         bot.random, bot.relashionships_enable, bot.relashionships_max_follow, bot.dont_like)
+        index = len(bots) - 1
+        if index < 0:
+            pass
+        else:
+            list_bots = []
+            if index == 0:
+                bots_i(0)
+            for i in range(index):
+                list_bots.append(gevent.spawn(bots_i(i)))
+
+            gevent.joinall(list_bots)
+            print(index)
