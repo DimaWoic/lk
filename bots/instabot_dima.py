36436bc855b0c7619059877248aa798a5bc3b3ee
Dmitrii <wdv85@mail.ru>
2020-10-23 19:44:34 +0400

    Дописан метод log_in класса InstaLikeBot начат метод like_by_tags

--- bots/instabot_dima.py
+++ bots/instabot_dima.py
@@ -11,38 +11,65 @@ from random import randrange
 
 
 class InstaLikeBot():
-    def login(self):
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
 
         path = os.getcwd()
         browser_options = Options()
-        browser_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 '
-                                     '(KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
-        driver = webdriver.Firefox(options=browser_options, executable_path=path + '/geckodriver')
-        wait = WebDriverWait(driver, 10)
-        driver.get(url='https://www.instagram.com/')
-        wait.until(expected_conditions.presence_of_element_located((By.XPATH,
+        browser_options.add_argument('--disable-notifications')
+        browser_options.add_argument(
+            '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 '
+            '(KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+        self.driver = webdriver.Firefox(options=browser_options, executable_path=path + '/geckodriver')
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get(url='https://www.instagram.com/')
+
+    def log_in(self):
+
+        if self.login == None or self.login == '':
+            print('Введите корректный логин')
+            exit()
+        if self.password == None or self.password == '':
+            print('Введите корректный пароль')
+            exit()
+
+
+        self.wait.until(expected_conditions.presence_of_element_located((By.XPATH,
             "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")))
-        username = driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
+        username = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
                                      '/div[2]/div[1]/div/form/div/div[1]/div/label/input')
         time.sleep(5)
-        username.send_keys('rumagpie')
-        password = driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
+
+        username.send_keys(self.login)
+        password = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
                                                 '/div[2]/div[1]/div/form/div/div[2]/div/label/input')
         time.sleep(5)
-        password.send_keys('456988wdv')
-        log_in = driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
+
+        password.send_keys(self.password)
+        log_in = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article'
                                               '/div[2]/div[1]/div/form/div/div[3]/button')
         log_in.click()
         time.sleep(10)
-        save_data = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
+        save_data = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
         save_data.click()
 
-        driver.implicitly_wait(15)
+        time.sleep(10)
 
-        popup = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-        ActionChains(driver).move_to_element(popup).click(popup)
+        ActionChains(self.driver).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).perform()
+
+    def like_by_tags(self):
+
+        profile = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/section/div[3]/div[1]/div/div'
+                                                       '/div[2]/div[1]/div/div/a')
+        profile.click()
+
+        search = self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')
+        search.send_keys('море')
+        search.send_keys(Keys.ENTER)
 
 
-
-bot = InstaLikeBot()
-bot.login()
+bot = InstaLikeBot(login='rumagpie', password='456988wdv')
+bot.log_in()
+bot.like_by_tags()
